b92570531f778170382d2d5cce196339cc245904
Lucas <joselucasrs@gmail.com>
2022-05-21 23:44:50 -0300

    Error Pages.

---
+++ src/routes/__error.svelte
@@ -0,0 +1,26 @@
+<script lang="ts" context="module">
+	import type { LoadInput } from '@sveltejs/kit';
+
+	export function load({ error }: LoadInput) {
+		return {
+			props: { message: error!.message }
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let message: string;
+</script>
+
+<div class="error">
+	<h2>Ooops</h2>
+	<p>{message}</p>
+	<a href="/guides">View all Guides...</a>
+</div>
+
+<style>
+	.error {
+		text-align: center;
+		margin-bottom: 60px;
+	}
+</style>
