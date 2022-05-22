c3f27e7e46ead921c811b8a1e10116a5fc78b9a9
Lucas <joselucasrs@gmail.com>
2022-05-21 21:49:13 -0300

    Pages & Routes.

---
+++ src/routes/about.svelte
@@ -0,0 +1,19 @@
+<div class="about">
+	<h2>About Us</h2>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi praesentium quis neque, est eum
+		fugiat totam recusandae ullam sed eos, pariatur quidem ut quaerat similique repudiandae
+		laudantium libero, laborum ipsa.
+	</p>
+
+	<a href="/guides">View Guides</a>
+	<a href="/">Home</a>
+</div>
+
+<style>
+	.about {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="index">
+	<h2>Welcome to SvelteKit</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur tenetur voluptatem, cum soluta
+		modi velit adipisci alias repellat non nobis suscipit totam obcaecati culpa, debitis odio
+		impedit. Ratione, velit hic!
+	</p>
+
+	<a href="/guides">View Guides</a>
+	<a href="/about">About</a>
+</div>
+
+<style>
+	.index {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
