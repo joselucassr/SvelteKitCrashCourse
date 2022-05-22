577ae9b981dd52721a90107f2ddd4860ddca41a6
Lucas <joselucasrs@gmail.com>
2022-05-21 23:09:00 -0300

    Loading Data & SSR.

---
+++ src/routes/guides/guideType.ts
@@ -0,0 +1,6 @@
+export interface Guide {
+	userId: number;
+	id: number;
+	title: string;
+	body: string;
+}
--- src/routes/guides/index.svelte
+++ src/routes/guides/index.svelte
@@ -1,7 +1,36 @@
+<script lang="ts" context="module">
+	import type { LoadInput } from '@sveltejs/kit';
+
+	export async function load({ fetch }: LoadInput) {
+		const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+		const guides = await res.json();
+
+		if (res.ok) {
+			return {
+				props: {
+					guides
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error('Could not fetch the guides')
+		};
+	}
+</script>
+
+<script lang="ts">
+	import type { Guide } from './guideType';
+
+	export let guides: Guide[];
+</script>
+
 <div class="guides">
 	<ul>
-		<li><a href="/">Guide 1</a></li>
-		<li><a href="/">Guide 2</a></li>
+		{#each guides as guide (guide.id)}
+			<li><a href="/">{guide.title}</a></li>
+		{/each}
 	</ul>
 </div>
 
